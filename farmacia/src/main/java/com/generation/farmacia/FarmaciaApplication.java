76ca1f597d2f5fc96abee899835384025577fd6d
johnnyalvesx <alvesjohnny@outlook.com>
2021-10-01 12:01:15 -0300

    Projeto farmacia no spring + identação projeto lojaGames

--- farmacia/src/main/java/com/generation/farmacia/FarmaciaApplication.java
+++ farmacia/src/main/java/com/generation/farmacia/FarmaciaApplication.java
@@ -1,13 +1,13 @@
-package com.generation.lojaGames;
+package com.generation.farmacia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LojaGamesApplication {
+public class FarmaciaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LojaGamesApplication.class, args);
+		SpringApplication.run(FarmaciaApplication.class, args);
 	}
 
 }
